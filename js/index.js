68f74d0dc946aa5a47107bc6dac1640f0e37520c
Melanie <melanie.loffbird@gmail.com>
2016-01-26 18:48:20 -0500

    lazyline working on homepage

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+
 $(document).ready(function() {
+
+    /**
+	* Guess Check
+	*/
 
 	// hide messages
 	$('#wrong').hide();
@@ -8,6 +13,7 @@ $(document).ready(function() {
 	var answer = document.getElementById('guess-input').name;
 
 
+	// check answer and hide boxes
 	function guessAnswer() {
 	  $("button.guess-submit").click(function(event) {
 
@@ -22,6 +28,7 @@ $(document).ready(function() {
 
 	}
 
+	//submit on enter
 	function enterSubmit() {
 	   $("#guess-input").keyup(function(event){
 	      if(event.keyCode == 13){
@@ -34,27 +41,24 @@ $(document).ready(function() {
 
 	enterSubmit();
 
+	/**
+	* LazyLine Painter
+	*/
 
-// /* Adapted from Jake Archibald's "Animated Line Drawing in SVG"
-// * http://jakearchibald.com/2013/animated-line-drawing-svg/
-// */
+	var $logo = $('#sketch');
 
+	      $logo.lazylinepainter({
+	          'svgData': svgData,
+	          'strokeWidth': 1,
+	          'speedMultiplier': 3,
+	          'strokeColor': '#b5287b',
+	              'drawSequential': true,
+	              'ease': 'easeInOutQuad'
+	      });
 
-	var path = document.querySelector('.content path');
-	var length = path.getTotalLength();
-
-	// Set up the starting positions
-	path.style.strokeDasharray = length + ' ' + length;
-	path.style.strokeDashoffset = length;
-
-	// Trigger a layout so styles are calculated & the browser
-	// picks up the starting position before animating
-	path.getBoundingClientRect();
-	// Define our transition
-	path.style.transition = path.style.WebkitTransition =
-	    'stroke-dashoffset 20s ease-in-out';
-	// Go!
-	path.style.strokeDashoffset = '0';
+          setTimeout(function(){
+              $logo.lazylinepainter('paint');
+          }, 10)
 
 
 
