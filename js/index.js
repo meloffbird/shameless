d0099a76445a7b026ed31975a076d2ab74516389
Melanie <melanie.loffbird@gmail.com>
2016-01-26 18:20:24 -0500

    fresh new js - answer input

--- js/index.js
+++ js/index.js
@@ -1,64 +1,68 @@
+$(document).ready(function() {
+
+	// hide messages
+	$('#wrong').hide();
+	$('#correct').hide();
+
+
+	var answer = document.getElementById('guess-input').name;
+
+
+	function guessAnswer() {
+	  $("button.guess-submit").click(function(event) {
+
+	    if ( $('#guess-input').val() == answer) {
+	      $('#correct').show();
+	      $('#wrong').hide();
+	    } else {
+	      $('#wrong').show();
+	    }
+
+	  });
+
+	}
+
+	function enterSubmit() {
+	   $("#guess-input").keyup(function(event){
+	      if(event.keyCode == 13){
+	          $("#guess-submit").click();
+	      }
+	   });
+
+	  guessAnswer();
+	}
+
+	enterSubmit();
+
+
+// /* Adapted from Jake Archibald's "Animated Line Drawing in SVG"
+// * http://jakearchibald.com/2013/animated-line-drawing-svg/
+// */
+
+
+	var path = document.querySelector('.content path');
+	var length = path.getTotalLength();
+
+	// Set up the starting positions
+	path.style.strokeDasharray = length + ' ' + length;
+	path.style.strokeDashoffset = length;
+
+	// Trigger a layout so styles are calculated & the browser
+	// picks up the starting position before animating
+	path.getBoundingClientRect();
+	// Define our transition
+	path.style.transition = path.style.WebkitTransition =
+	    'stroke-dashoffset 20s ease-in-out';
+	// Go!
+	path.style.strokeDashoffset = '0';
 
 
 
-/* Adapted from Jake Archibald's "Animated Line Drawing in SVG"
-* http://jakearchibald.com/2013/animated-line-drawing-svg/
-*/
-
-var path = document.querySelector('.content path');
-var length = path.getTotalLength();
-
-// Set up the starting positions
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
-
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-path.getBoundingClientRect();
-// Define our transition
-path.style.transition = path.style.WebkitTransition =
-    'stroke-dashoffset 20s ease-in-out';
-// Go!
-path.style.strokeDashoffset = '0';
+});
 
 
 
 
-
-
-/* Check answer to questions */
-
-
-  // DEFAULT SETTINGS
-  $('#wrong').hide();
-  $('#correct').hide();
-
-  		console.log( "ready!" );
-
-  // SELECT INPUT BOX + CLEAR AFTER WRONG ANSWER
-  $('input.superhero').focus(function () {
-      $('#wrong').hide();
-      $('#correct').hide();
-      $("button.checkanswer").show();
-  });
-
-  // CHECK ANSWER
-  $("button.checkanswer").click(function () {
-
-      var superhero = $('input.superhero').val();
-
-  //IF CORRECT ANSWER
-     if ($('input.superhero').val() == 'Owl') {
-          $('#correct').show();
-
-  //IF WRONG ANSWER
-      } else {
-          $("button.checkanswer").hide();
-          $('#wrong').show();
-          $("button.checkanswer").show();
-      }
-
-  });
 
 
 
